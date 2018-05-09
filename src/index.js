8bb15980d4c6a48160ad64db7ee5f43c89d45d13
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-05-08 21:05:52 -0400

    Started homepage and added routes for other pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
+import './main.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { browserHistory, Router, Route } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Layout from './modules/containers/layout';
+import Home from './modules/containers/home';
+import Reservations from './modules/containers/reservations';
+import Packages from './modules/containers/packages'
+import Photos from './modules/containers/photos';
+import FAQ from './modules/containers/faq';
+import Contact from './modules/containers/contact';
+
+import reducers from './modules/reducers';
+
+const store = createStore(reducers, composeWithDevTools (
+    applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Home} />
+                <Route path='/reservations' component={Reservations} />
+                <Route path='/packages' component={Packages} />
+                <Route path='/photos' component={Photos} />
+                <Route path='/faq' component={FAQ} />
+                <Route path='/contact' component={Contact} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
