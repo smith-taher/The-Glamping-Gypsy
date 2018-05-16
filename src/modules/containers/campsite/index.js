47917223a40d8c9489a0f3653140b02675816c90
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-05-16 15:52:43 -0400

    render product page when more info button is pushed

--- src/modules/containers/campsite/index.js
+++ src/modules/containers/campsite/index.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import R from 'ramda';
 
 import { fetchCampsiteById } from '../../actions';
+import { getCampsiteById } from '../../selectors';
 
 class Campsite extends Component {
     componentDidMount () {
         this.props.fetchCampsiteById(this.props.params.id)
     }
-    render () {
+
+    renderContent () {
+        const {campsite} = this.props
         return (
-            <div>
-                Campsite
+            <div className='thumbnail'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <img
+                            className='img-thumbnail'
+                            src={campsite.image}
+                            alt={campsite.name}
+                        />
+                    </div>
+                    {/* <div className='col-md-6'>
+                        Our fields
+                    </div> */}
+                </div>
+                <div className='caption-full'>
+                    <h4 className='pull-right'>
+                        ${campsite.price}
+                    </h4>
+                    <h4>
+                        {campsite.name}
+                    </h4>
+                    <p>
+                        {campsite.tentDescription}
+                    </p>
+                </div>
             </div>
         )
+    }
+
+    render () {
+        const { campsite } = this.props
+        return (
+            <div className='view-container'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-md-12'>
+                            {campsite && this.renderContent()}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        campsite: getCampsiteById(state, state.campsitePage.id)
     }
 }
 
@@ -20,4 +67,4 @@ const mapDispatchToProps = {
     fetchCampsiteById
 }
 
-export default connect(null, mapDispatchToProps)(Campsite);
+export default connect(mapStateToProps, mapDispatchToProps)(Campsite);
